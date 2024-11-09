9cea6040fedbd85c5106c2a1e686e83d6df96b7e
JhonnyGames <borozdinaes89@gmail.com>
2024-11-09 13:11:49 +0500

    deleted method

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,6 +2,60 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter folder path for clean: ");
+        string path = Console.ReadLine();
+
+
+        DeleteFilesAndFolders(path);
+    }
+
+    public static void DeleteFilesAndFolders(string path)
+    {
+        try
+        {
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine($"Path {path} doesnt exist.");
+                return;
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo[] folders = dir.GetDirectories();
+            FileInfo[] files = dir.GetFiles();
+
+
+            foreach (DirectoryInfo folder in folders)
+            {
+                try
+                {
+                    folder.Delete(true);
+                    Console.WriteLine($"folder ({folder.Name}) deleted sucsessfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Folder {folder.Name} isn't deleted, error {ex.Message}");
+                }
+                
+            }
+            foreach (FileInfo file in files)
+            {
+                try
+                {
+                    file.Delete();
+                    Console.WriteLine($"file ({file.Name}) deleted sucsessfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"File {file.Name} isn't deleted, error {ex.Message}");
+                }
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("error: " + ex.Message);
+        }
+
+        Console.WriteLine($"Cleaning completed.");
     }
 }
