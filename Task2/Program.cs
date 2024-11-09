a4011b17c3f31e7afa6c4f7e24a9ee96c9f5e3e0
JhonnyGames <borozdinaes89@gmail.com>
2024-11-09 13:23:59 +0500

    empty method FolderSize

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!"); 
+        Console.WriteLine("Enter path to canculate size: ");
+        string path = Console.ReadLine();
+
+        FolderSize(path);
+    }
+
+    public static long FolderSize(string path)
+    {
+        long size = 0;
+
+
+        return size;
     }
 }
