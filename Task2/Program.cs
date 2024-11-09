f2eb1182a35a39e9b7926d13ae4ca8733680335b
JhonnyGames <borozdinaes89@gmail.com>
2024-11-09 13:39:51 +0500

    method CalcFolderSize+

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,13 +5,25 @@
         Console.WriteLine("Enter path to canculate size: ");
         string path = Console.ReadLine();
 
-        FolderSize(path);
+        Console.WriteLine(CalcFolderSize(Directory.CreateDirectory(path)));
     }
 
-    public static long FolderSize(string path)
+    public static long CalcFolderSize(DirectoryInfo dir)
     {
         long size = 0;
 
+        FileInfo[] files = dir.GetFiles();
+
+        foreach (FileInfo file in files)
+        {
+            size += file.Length;
+        }
+
+        DirectoryInfo[] folders = dir.GetDirectories();
+        foreach (DirectoryInfo folder in folders)
+        {
+            size += CalcFolderSize(folder);
+        }
 
         return size;
     }
