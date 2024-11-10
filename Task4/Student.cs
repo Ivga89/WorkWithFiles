3ff39e1a6b5a0fd74026f9230396d9115c57ded2
JhonnyGames <borozdinaes89@gmail.com>
2024-11-10 13:27:05 +0500

    Task4 copy ex code

---
+++ Task4/Student.cs
@@ -0,0 +1,7 @@
+﻿internal class Student
+{
+    public string Name { get; set; }
+    public string Group { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public decimal AverageScore { get; set; }
+}
